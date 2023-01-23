fda916895940170c9dd7cbf0c0befeadfcaee0b6
Yaschenko Yaroslav <yaschenko.ya.yu@gmail.com>
2023-01-23 22:27:37 +0200

    Add Grab-Drop interaction

---
+++ Assets/ReachBasket.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReachBasket : MonoBehaviour
+{
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        IKControl.Control.Drop(collision.transform);
+    }*/
+}
--- Assets/Scripts/IKControl.cs
+++ Assets/Scripts/IKControl.cs
@@ -5,16 +5,65 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class IKControl : MonoBehaviour
 {
-    protected Animator animator;
+    protected static Animator animator;
 
+    public static IKControl Control;
+    
     public bool ikActive = false;
+    public static bool isGrab = false;
+    public bool isNear = false;
+
     public Transform rightHandObj = null;
+    public Transform leftHandObj = null;
     public Transform lookObj = null;
+    public Transform dropPoint = null;
+
+    public float middleDistance = 0.5f;
+    public float nearDistance = 1;
+    //float smooth = 0;
+    [Range(0,1)]
+    public float speed = 0.1f;
+    
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        if (Control==null)
+        {
+            Control = this;
+        }
     }
+
+    private void Update()
+    {
+        if (rightHandObj != null)
+        {
+            if (!isGrab)
+            {
+                if (IsObjectNear(nearDistance))
+                {
+                    Grab(rightHandObj.parent);
+                }
+                else if (IsObjectNear(middleDistance) && !isNear)
+                {
+                    Near();
+                }
+            }
+            else
+            {
+                if ((rightHandObj.parent.position - dropPoint.position).sqrMagnitude < nearDistance * nearDistance)
+                {
+                    Drop(rightHandObj.parent);
+                }
+                else
+                {
+                    rightHandObj.parent.position = Vector3.Lerp(rightHandObj.parent.position, dropPoint.position, speed * Time.deltaTime);
+                }
+            }
+        }
+    }
+
+   
 
     //a callback for calculating IK
     void OnAnimatorIK()
@@ -38,8 +87,20 @@ public class IKControl : MonoBehaviour
                 {
                     animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
                     animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
-                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, speed);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, speed);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position); //Vector3.Lerp(animator.GetIKPosition(AvatarIKGoal.RightHand), rightHandObj.position,speed*Time.deltaTime));
                     animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+                    //smooth += Mathf.Clamp(speed * Time.deltaTime,0,1);
+                }
+                if (leftHandObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+
+                    animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandObj.position); 
+                    animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandObj.rotation);
                 }
 
             }
@@ -47,10 +108,48 @@ public class IKControl : MonoBehaviour
             //if the IK is not active, set the position and rotation of the hand and head back to the original position
             else
             {
+                //smooth = 0;
                 animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
                 animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
                 animator.SetLookAtWeight(0);
             }
         }
+    }
+    public bool IsObjectNear(float distance)
+    {
+        if ((animator.GetBoneTransform(HumanBodyBones.RightHand).position - rightHandObj.position).sqrMagnitude < distance*distance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void Near()
+    {
+        isNear = true;
+        animator.SetTrigger("Near");
+    }
+
+    public void Grab(Transform fruit)
+    {
+        //rightHandObj.parent.GetComponent<Rigidbody>().isKinematic = true;
+        //rightHandObj.parent = animator.GetBoneTransform(HumanBodyBones.RightHand);
+        // if () ;
+        fruit.GetComponent<Rigidbody>().isKinematic = true;
+        //fruit = animator.GetBoneTransform(HumanBodyBones.RightHand);
+        isGrab = true;
+        animator.SetTrigger("Drop");
+    }
+    
+    public /*static*/ void Drop(Transform fruit)
+    {
+        fruit.GetComponent<Rigidbody>().isKinematic = false;
+
+        rightHandObj = null;
+        isGrab = false;
+        isNear = false;
+        animator.SetTrigger("Idle");
     }
 }
